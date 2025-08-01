45734bb02a08b326923045c8cbb71562a3414f9f
twopic2 <bowenzhang0@gmail.com>
2025-08-01 16:54:24 -0700

    Improved version that allows for direct io rather than using oses page cache

--- operations.go
+++ operations.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"os"
+	"strconv"
 	"sync"
+	"sync/atomic"
 	"time"
+
+	"github.com/ncw/directio"
 )
 
 type writeResults struct {
@@ -19,21 +23,46 @@ type readResults struct {
 	Error     error
 }
 
+var fileCounter int64
+
+func alignDataForDirectIO(data []byte) []byte {
+	blockSize := directio.BlockSize
+	requiredSize := ((len(data) + blockSize - 1) / blockSize) * blockSize
+	alignedData := directio.AlignedBlock(requiredSize)
+	copy(alignedData, data)
+
+	for i := len(data); i < requiredSize; i++ {
+		alignedData[i] = 0
+	}
+
+	return alignedData
+}
+
 func write(data []byte, key []byte, file string) (time.Duration, float64, error) {
 	start := time.Now()
 
 	encryptedData, err := encrypt(data, key)
-	checkError(err)
+	if err != nil {
+		return 0, 0, err
+	}
 
-	f, err := os.OpenFile(file, os.O_CREATE|os.O_WRONLY|os.O_SYNC, 0644)
-	checkError(err)
-	defer f.Close()
+	alignedData := alignDataForDirectIO(encryptedData)
 
-	_, err = f.Write(encryptedData)
-	checkError(err)
+	oFile, err := directio.OpenFile(file, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		return 0, 0, err
+	}
+	defer oFile.Close()
 
-	err = f.Sync()
-	checkError(err)
+	_, err = oFile.Write(alignedData)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	err = oFile.Sync()
+	if err != nil {
+		return 0, 0, err
+	}
 
 	duration := time.Since(start)
 	bytesTime := float64(len(encryptedData))
@@ -41,27 +70,38 @@ func write(data []byte, key []byte, file string) (time.Duration, float64, error)
 	return duration, bytesTime, nil
 }
 
-func writeConcurrent(data []byte, key []byte, file string, wg *sync.WaitGroup, resultChannel chan<- writeResults) {
+func writeConcurrent(data []byte, key []byte, wg *sync.WaitGroup, resultChannel chan<- writeResults) {
 	defer wg.Done()
 
 	start := time.Now()
 
 	encryptedData, err := encrypt(data, key)
-	checkError(err)
+	if err != nil {
+		resultChannel <- writeResults{Error: err}
+		return
+	}
 
-	writenfiles := file + "." + string(rune(start.UnixNano()))
+	counter := atomic.AddInt64(&fileCounter, 1)
+	writtenFiles := "tmp" + strconv.FormatInt(counter, 10)
 
-	f, err := os.OpenFile(writenfiles, os.O_CREATE|os.O_WRONLY|os.O_SYNC, 0644)
-	checkError(err)
+	alignedData := alignDataForDirectIO(encryptedData)
 
-	defer f.Close()
-	defer os.Remove(writenfiles)
+	f, err := directio.OpenFile(writtenFiles, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		resultChannel <- writeResults{Error: err}
+		return
+	}
 
-	_, err = f.Write(encryptedData)
-	checkError(err)
+	defer func() {
+		f.Close()
+		os.Remove(writtenFiles)
+	}()
 
-	err = f.Sync()
-	checkError(err)
+	_, err = f.Write(alignedData)
+	if err != nil {
+		resultChannel <- writeResults{Error: err}
+		return
+	}
 
 	duration := time.Since(start)
 	bytesTime := float64(len(encryptedData))
@@ -76,22 +116,44 @@ func writeConcurrent(data []byte, key []byte, file string, wg *sync.WaitGroup, r
 func read(file string, key []byte) (time.Duration, float64, []byte, error) {
 	start := time.Now()
 
-	f, err := os.OpenFile(file, os.O_RDONLY|os.O_SYNC, 0)
-	checkError(err)
-	defer f.Close()
+	oFile, err := directio.OpenFile(file, os.O_RDONLY, 0)
+	if err != nil {
+		return 0, 0, nil, err
+	}
+	defer oFile.Close()
 
-	stat, err := f.Stat()
-	checkError(err)
+	stat, err := oFile.Stat()
+	if err != nil {
+		return 0, 0, nil, err
+	}
 
-	encryptedData := make([]byte, stat.Size())
-	_, err = f.Read(encryptedData)
-	checkError(err)
+	fileSize := int(stat.Size())
+	alignedBuffer := directio.AlignedBlock(fileSize)
 
-	decryptData, err := decrypt(encryptedData, key)
-	checkError(err)
+	n, err := oFile.Read(alignedBuffer)
+	if err != nil {
+		return 0, 0, nil, err
+	}
+
+	actualSize := n
+	for i := n - 1; i >= 0; i-- {
+		if alignedBuffer[i] != 0 {
+			actualSize = i + 1
+			break
+		}
+	}
+
+	encryptedDatas := alignedBuffer[:actualSize]
+	decryptData, err := decrypt(encryptedDatas, key)
+	if err != nil {
+		decryptData, err = decrypt(alignedBuffer[:n], key)
+		if err != nil {
+			return 0, 0, nil, err
+		}
+	}
 
 	duration := time.Since(start)
-	bytesTime := float64(len(encryptedData))
+	bytesTime := float64(len(encryptedDatas))
 
 	return duration, bytesTime, decryptData, nil
 }
@@ -101,19 +163,46 @@ func readConcurrent(file string, key []byte, wg *sync.WaitGroup, resultChannel c
 
 	start := time.Now()
 
-	f, err := os.OpenFile(file, os.O_RDONLY|os.O_SYNC, 0)
-	checkError(err)
-	defer f.Close()
+	oFile, err := directio.OpenFile(file, os.O_RDONLY, 0)
+	if err != nil {
+		resultChannel <- readResults{Error: err}
+		return
+	}
+	defer oFile.Close()
 
-	stat, err := f.Stat()
-	checkError(err)
+	stat, err := oFile.Stat()
+	if err != nil {
+		resultChannel <- readResults{Error: err}
+		return
+	}
 
-	encryptedData := make([]byte, stat.Size())
-	_, err = f.Read(encryptedData)
-	checkError(err)
+	fileSize := int(stat.Size())
+	alignedBuffer := directio.AlignedBlock(fileSize)
+
+	n, err := oFile.Read(alignedBuffer)
+	if err != nil {
+		resultChannel <- readResults{Error: err}
+		return
+	}
+
+	actualSize := n
+	for i := n - 1; i >= 0; i-- {
+		if alignedBuffer[i] != 0 {
+			actualSize = i + 1
+			break
+		}
+	}
+
+	encryptedData := alignedBuffer[:actualSize]
 
 	decryptedData, err := decrypt(encryptedData, key)
-	checkError(err)
+	if err != nil {
+		decryptedData, err = decrypt(alignedBuffer[:n], key)
+		if err != nil {
+			resultChannel <- readResults{Error: err}
+			return
+		}
+	}
 
 	duration := time.Since(start)
 	bytesTime := float64(len(encryptedData))
@@ -130,11 +219,14 @@ func latency(data []byte, key []byte, file string) (time.Duration, error) {
 	start := time.Now()
 
 	_, _, err := write(data, key, file)
-
-	checkError(err)
+	if err != nil {
+		return 0, err
+	}
 
 	_, _, _, err = read(file, key)
-	checkError(err)
+	if err != nil {
+		return 0, err
+	}
 
 	return time.Since(start), nil
 }
